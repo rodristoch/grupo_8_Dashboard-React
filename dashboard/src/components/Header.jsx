0d6ba7e3a002a002583639689e4a011823313cbd
Agustin Macko <mackoaa@gmail.com>
2024-03-23 22:54:32 -0300

    modificado tampoño logo en header

--- dashboard/src/components/Header.jsx
+++ dashboard/src/components/Header.jsx
@@ -6,10 +6,10 @@ function Header() {
       <Container className="text-center mt-4 mx-8">
         <Row>
           <Col xs={1}>
-            <h3>Dashboard</h3>
+            <h4>Dashboard</h4>
           </Col>
           <Col xs={11} className="d-flex justify-content-end">
-            <img src={LogoHead} className="icono" alt="Icono" style={{ width: '100px', height: '50px' }} /> {/* Utiliza la imagen importada */}
+            <img src={LogoHead} className="icono" alt="Icono" style={{ width: '100px', height: '40px' }} /> {/* Utiliza la imagen importada */}
           </Col>
         </Row>
       </Container>
