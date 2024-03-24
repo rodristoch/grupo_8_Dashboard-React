38cee9e564134f68ce39c26a64d7520bd4e7bd87
Agustin Macko <mackoaa@gmail.com>
2024-03-23 22:52:11 -0300

    Agregada funcionalidad de location en el navbar

--- dashboard/src/Components/Navbar.jsx
+++ dashboard/src/Components/Navbar.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom'; // Asegúrate de importar Link de react-router-dom
+import { Link, useLocation } from 'react-router-dom';
 
 function Navbar() {
+  const location = useLocation();
+
   return (
     <Container className="text-center mt-4 mx-8">
       <Nav variant="tabs" defaultActiveKey="/home">
         <Nav.Item>
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/" active={location.pathname === '/'}>Home</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link as={Link} to="/users">Usuarios</Nav.Link>
+          <Nav.Link as={Link} to="/users" active={location.pathname === '/users'}>Usuarios</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link as={Link} to="/products">Productos</Nav.Link>
+          <Nav.Link as={Link} to="/products" active={location.pathname === '/products'}>Productos</Nav.Link>
         </Nav.Item>
       </Nav>
     </Container>
